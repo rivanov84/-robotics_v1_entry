b3bd1e84f8d4fbf4473d84e51f99ea835a3516a2
rivanov84 <49672682+rivanov84@users.noreply.github.com>
2022-08-31 22:57:26 +0300

    Update solution.py

--- python/solution.py
+++ python/solution.py
@@ -16,7 +16,6 @@ from utils.function_tracer import FunctionTracer
 
 def compute_solution(images: List[Union[PackedImage, StrideImage]]):
     ft = FunctionTracer("compute_solution", "seconds")
-
-    #TODO fill solution
+    for image in images:
+       image.pixels_red = [x - 150 if x>=200 else x for x in image.pixels_red]
     del ft
-            
